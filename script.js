e95eaa4d226792c8555bb228a3e0fb209f57e728
TioDaniloBR <gothic.joker@hotmail.com>
2020-05-24 12:12:36 -0300

    formulário de contato com css para estilizar e java para funcionar um botão criado

---
+++ script.js
@@ -0,0 +1,27 @@
+//pego o elemento com id de botão e salvo na variavel botao
+var botao = document.querySelector('#botao')
+
+//adiciona um eventlistener no botao com uma função dentro
+botao.addEventListener('click',()=>{
+	
+	//pega os elementos com ida nome e email e salva nas variaveis respectivas
+	var nome = document.querySelector('#nome').value
+	var email = document.querySelector('#email').value
+
+	console.log(nome,email)
+
+	//cria um JSON com os dados coletados dos inputs
+	var contatoJson = {
+		"nome": nome,
+		"email": email
+	}
+
+	console.log(contatoJson)
+
+	//aciona o submit do formulário
+	document.querySelector('#formulario').submit()
+	
+})
+
+var teste = prompt('Digite seu nome:');
+console.log(teste)
